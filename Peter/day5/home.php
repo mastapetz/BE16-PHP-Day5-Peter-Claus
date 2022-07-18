a1cca3e4267e4e67a74da93e02633e970b8a4853
OinkoIT <claus.mersch@gmail.com>
2022-07-18 12:54:48 +0200

    home index

--- Peter/day5/home.php
+++ Peter/day5/home.php
@@ -1,6 +1,31 @@
 <?php
+
+// INDEX ALT
+
 session_start();
 require_once 'components/db_connect.php';
+
+$sql = "SELECT * FROM dishes";
+$result = mysqli_query($connect ,$sql);
+$tbody=''; //this variable will hold the body for the table
+if(mysqli_num_rows($result)  > 0) {     
+    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){         
+        $tbody .= "<tr>
+            <td><img class='img-thumbnail' src='pictures/" .$row['image']."'</td>
+            <td>" .$row['name']."</td>
+            <td>" .$row['price']."</td>
+            <td><a href='update.php?id=" .$row['dishID']."'><button class='btn btn-primary btn-sm' type='button'>Edit</button></a>
+            <a href='delete.php?id=" .$row['dishID']."'><button class='btn btn-danger btn-sm' type='button'>Delete</button></a></td>
+            </tr>";
+    };
+} else {
+    $tbody =  "<tr><td colspan='5'><center>No Data Available </center></td></tr>";
+}
+
+
+// HOME
+
+
 
 // if adm will redirect to dashboard
 if (isset($_SESSION['adm'])) {
@@ -38,6 +63,20 @@ mysqli_close($connect);
             background: rgb(2, 0, 36);
             background: linear-gradient(24deg, rgba(2, 0, 36, 1) 0%, rgba(0, 212, 255, 1) 100%);
         }
+        .manageProduct {           
+                margin: auto;
+            }
+            .img-thumbnail {
+                width: 70px !important;
+                height: 70px !important;
+            }
+            td {          
+                text-align: left;
+                vertical-align: middle;
+            }
+            tr {
+                text-align: center;
+            }
     </style>
 </head>
 
@@ -50,5 +89,27 @@ mysqli_close($connect);
         <a href="logout.php?logout">Sign Out</a>
         <a href="update.php?id=<?php echo $_SESSION['user'] ?>">Update your profile</a>
     </div>
+
+    <br><br><br>
+
+    <div class="manageProduct w-75 mt-3">    
+            <div class='mb-3'>
+                <a href= "create.php"><button class='btn btn-primary'type="button" >Add product</button></a>
+            </div>
+            <p class='h2'>Products</p>
+            <table class='table table-striped'>
+                <thead class='table-success'>
+                    <tr>
+                        <th>Picture</th>
+                        <th>Name</th>
+                        <th>price</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?= $tbody;?>
+                </tbody>
+            </table>
+        </div>
 </body>
 </html>
